df3c6fd0a10a69d22f55f2ece857aef4f8e47b16
Jbertalli <jjbertalli@berkeley.edu>
2022-09-05 23:04:24 -0700

    fix localStorage (just change key names)

--- components/localStorage.tsx
+++ components/localStorage.tsx
@@ -3,80 +3,81 @@ import React, { useEffect } from "react";
 const LOCAL_STORAGE_KEY_ZIP = 'UserZip';
 const LOCAL_STORAGE_KEY_CITY = 'UserCity';
 const LOCAL_STORAGE_KEY_LAT = 'UserLatitude';
-const LOCAL_STORAGE_KEY_LONG = 'UserLongitude';
+const LOCAL_STORAGE_KEY_LON = 'UserLongitude';
 const LOCAL_STORAGE_KEY_STATE = 'UserState';
 const LOCAL_STORAGE_KEY_STATE_ABBR = 'UserStateAbbr';
 
 export default function Local({ setZip, zip, setCity, city, latCoord, setLatCoord, longCoord, setLongCoord, state, setState, stateAbbreviation, setStateAbbreviation }) {
-    // zip
-    // useEffect(() => {
-    //     const storedZip = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_ZIP))
-    //     if (storedZip) setZip(storedZip)
-    //   }, []);
 
-    // useEffect(() => {
-    //   localStorage.setItem(LOCAL_STORAGE_KEY_ZIP, 
-    //   JSON.stringify(zip))
-    // }, [zip]);
+  // zip
+  useEffect(() => {
+    const storedZip = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_ZIP))
+    if (storedZip) setZip(storedZip)
+  }, [])
 
-    // city
-    useEffect(() => {
-      const storedCity = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_CITY))
-      if (storedCity) setCity(storedCity)
-    }, [])
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_ZIP, 
+    JSON.stringify(zip))
+  }, [zip]);
 
-    useEffect(() => {
-      localStorage.setItem(LOCAL_STORAGE_KEY_CITY, 
-      JSON.stringify(city))
-    }, [city]);
+  // city
+  useEffect(() => {
+    const storedCity = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_CITY))
+    if (storedCity) setCity(storedCity)
+  }, [])
 
-    //latitude
-    useEffect(() => {
-      const storedLat = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_LAT))
-      if (storedLat) setLatCoord(storedLat)
-    }, [])
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_CITY, 
+    JSON.stringify(city))
+  }, [city]);
 
-    useEffect(() => {
-      localStorage.setItem(LOCAL_STORAGE_KEY_LAT, 
-      JSON.stringify(latCoord))
-    }, [latCoord]);
+  // latitude
+  useEffect(() => {
+    const storedLat = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_LAT))
+    if (storedLat) setLatCoord(storedLat)
+  }, [])
 
-    //longitude
-    // useEffect(() => {
-    //   const storedLong = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_LONG))
-    //   if (storedLong) setLongCoord(storedLong)
-    // }, [])
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_LAT, 
+    JSON.stringify(latCoord))
+  }, [latCoord]);
 
-    // useEffect(() => {
-    //   localStorage.setItem(LOCAL_STORAGE_KEY_LONG, 
-    //   JSON.stringify(longCoord))
-    // }, [longCoord]);
+  // longitude
+  useEffect(() => {
+    const storedLong = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_LON))
+    if (storedLong) setLongCoord(storedLong)
+  }, [])
 
-    // state
-    useEffect(() => {
-      const storedState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_STATE))
-      if (storedState) setState(storedState)
-    }, [])
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_LON, 
+    JSON.stringify(longCoord))
+  }, [longCoord]);
 
-    useEffect(() => {
-      localStorage.setItem(LOCAL_STORAGE_KEY_STATE, 
-      JSON.stringify(state))
-    }, [state]);
+  // state
+  useEffect(() => {
+    const storedState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_STATE))
+    if (storedState) setState(storedState)
+  }, [])
 
-    //state abbreviation
-    useEffect(() => {
-      const storedStateAbbr = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_STATE_ABBR))
-      if (storedStateAbbr) setStateAbbreviation(storedStateAbbr)
-    }, [])
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_STATE, 
+    JSON.stringify(state))
+  }, [state]);
 
-    useEffect(() => {
-      localStorage.setItem(LOCAL_STORAGE_KEY_STATE_ABBR, 
-      JSON.stringify(stateAbbreviation))
-    }, [stateAbbreviation]);
+  // state abbreviation
+  useEffect(() => {
+    const storedStateAbbr = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_STATE_ABBR))
+    if (storedStateAbbr) setStateAbbreviation(storedStateAbbr)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_STATE_ABBR, 
+    JSON.stringify(stateAbbreviation))
+  }, [stateAbbreviation]);
 
   return (
-      <>
-          &nbsp;
-      </>
+    <>
+        &nbsp;
+    </>
   );
 }
