b58184d5069986d0a2e9b0e70b46f8dd1818adb6
Jbertalli <jjbertalli@berkeley.edu>
2022-09-15 14:40:39 -0700

    antinode title

--- pages/antinode_map.tsx
+++ pages/antinode_map.tsx
@@ -9,10 +9,18 @@ export default function Antinode() {
                 <title>Antinode</title>
                 <meta name="description" content="antinode" />
             </Head>
-            <div style={{ position: 'absolute' }}>
+            {/* <div style={{ position: 'absolute' }}>
                 <Button style={{ background: 'blue', color: 'white', width: '50vw', height: '5vh' }}>
                     Antinode
                 </Button>
+            </div> */}
+            <div>
+                <div style={{ color: 'red', position: 'absolute', fontSize: '50px' }}>
+                    Current Location
+                </div>
+                <div style={{ color: 'red', position: 'absolute', fontSize: '50px', transform: 'translate(50vw)' }}>
+                    Antinode Location
+                </div>
             </div>
             <div style={{ transform: 'translateY(-411px)' }}>
                 <div style={{ position: 'absolute', zIndex: '1000000000', width: '50%', height: '100%' }}>
