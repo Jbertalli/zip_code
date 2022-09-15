998777750b5aa498cf49e4ccd13d3fe7760e6986
Jbertalli <jjbertalli@berkeley.edu>
2022-09-15 13:19:30 -0700

    antinode position

--- pages/antinode_map.tsx
+++ pages/antinode_map.tsx
@@ -3,11 +3,13 @@ import Map from '../components/map';
 export default function Antinode() {
     return (
         <>
-            <div style={{ position: 'absolute', zIndex: '1000000000', width: '50%', height: '100%' }}>
-                <Map />
-            </div>
-            <div style={{ position: 'absolute', zIndex: '1000000000', width: '50%', height: '100%', transform: 'translate(100%)' }}>
-                <Map />
+            <div style={{ transform: 'translateY(-371px)' }}>
+                <div style={{ position: 'absolute', zIndex: '1000000000', width: '50%', height: '100%' }}>
+                    <Map />
+                </div>
+                <div style={{ position: 'absolute', zIndex: '1000000000', width: '50%', height: '100%', transform: 'translate(100%)' }}>
+                    <Map />
+                </div>
             </div>
         </>
     );
