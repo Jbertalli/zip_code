741fdc19ea9800da2e754e1f72e22017ce6005de
Jbertalli <jjbertalli@berkeley.edu>
2022-09-15 13:26:03 -0700

    antinode header

--- pages/antinode_map.tsx
+++ pages/antinode_map.tsx
@@ -8,6 +8,9 @@ export default function Antinode() {
                 <title>Antinode</title>
                 <meta name="description" content="antinode" />
             </Head>
+            <div style={{ position: 'absolute' }}>
+                Antinode
+            </div>
             <div style={{ transform: 'translateY(-371px)' }}>
                 <div style={{ position: 'absolute', zIndex: '1000000000', width: '50%', height: '100%' }}>
                     <Map />
