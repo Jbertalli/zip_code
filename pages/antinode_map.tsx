7c7f16c11507a990d1839932a8e084b6ad6a594b
Jbertalli <jjbertalli@berkeley.edu>
2022-09-15 13:11:06 -0700

    antinode page

---
+++ pages/antinode_map.tsx
@@ -0,0 +1,14 @@
+import Map from '../components/map';
+
+export default function Antinode() {
+    return (
+        <>
+            <div style={{ position: 'absolute', zIndex: '1000000000', width: '50%', height: '100%' }}>
+                <Map />
+            </div>
+            <div style={{ position: 'absolute', zIndex: '1000000000', width: '50%', height: '100%', transform: 'translate(100%)' }}>
+                <Map />
+            </div>
+        </>
+    );
+}
