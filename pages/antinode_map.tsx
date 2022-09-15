2503366e24bb6b04c3f1094cacfd91bd3a30e4d0
Jbertalli <jjbertalli@berkeley.edu>
2022-09-15 13:21:20 -0700

    antinode head

--- pages/antinode_map.tsx
+++ pages/antinode_map.tsx
@@ -1,8 +1,13 @@
+import Head from 'next/head';
 import Map from '../components/map';
 
 export default function Antinode() {
     return (
         <>
+            <Head>
+                <title>Antinode</title>
+                <meta name="description" content="antinode" />
+            </Head>
             <div style={{ transform: 'translateY(-371px)' }}>
                 <div style={{ position: 'absolute', zIndex: '1000000000', width: '50%', height: '100%' }}>
                     <Map />
