082973b70e2ce549bc93fa11fc126ac2134f627d
Jbertalli <jjbertalli@berkeley.edu>
2022-09-15 21:05:59 -0700

    Antinode components

--- pages/antinode_map.tsx
+++ pages/antinode_map.tsx
@@ -7,56 +7,60 @@ import styles from '../styles/zip.module.css';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
 // import Map from '../components/map';
+import NodeMap from '../components/Nodemap';
+import AntinodeMap from '../components/Antinodemap';
 
-const libraries: string[] = ["places"];
-const mapContainerStyle = {
-  height: "95vh",
-  width: "100vw",
-};
+// const libraries: string[] = ["places"];
+// const mapContainerStyle = {
+//   height: "95vh",
+//   width: "100vw",
+//     // height: "50vh",
+//     // width: "100vw",
+// };
 
-const options = {
-  disableDefaultUI: true,
-  zoomControl: true,
-};
+// const options = {
+//   disableDefaultUI: true,
+//   zoomControl: true,
+// };
 
-const center = {
-  lat: 40.73,
-  lng: -73.93
-};
+// const center = {
+//   lat: 40.73,
+//   lng: -73.93
+// };
 
 export default function Antinode() {
 
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: process.env.MAPS_API_KEY,
-        libraries,
-      });
+    // const { isLoaded, loadError } = useLoadScript({
+    //     googleMapsApiKey: process.env.MAPS_API_KEY,
+    //     libraries,
+    //   });
     
-      const [markers, setMarkers] = useState([]);
-      const [selected, setSelected] = useState(null);
+    //   const [markers, setMarkers] = useState([]);
+    //   const [selected, setSelected] = useState(null);
     
-      const onMapClick = useCallback((e) => {
-        setMarkers((current) => [
-          ...current,
-          {
-            lat: e.latLng.lat(),
-            lng: e.latLng.lng(),
-            time: new Date(),
-          },
-        ]);
-      }, []);
+    //   const onMapClick = useCallback((e) => {
+    //     setMarkers((current) => [
+    //       ...current,
+    //       {
+    //         lat: e.latLng.lat(),
+    //         lng: e.latLng.lng(),
+    //         time: new Date(),
+    //       },
+    //     ]);
+    //   }, []);
     
-      const mapRef = useRef();
-      const onMapLoad = useCallback((map) => {
-        mapRef.current = map;
-      }, []);
+    //   const mapRef = useRef();
+    //   const onMapLoad = useCallback((map) => {
+    //     mapRef.current = map;
+    //   }, []);
     
-      const panTo = useCallback(({ lat, lng }) => {
-        mapRef.current.panTo({ lat, lng });
-        mapRef.current.setZoom(14);
-      }, []);
-    
-      if (loadError) return "Error";
-      if (!isLoaded) return "Loading...";
+    //   const panTo = useCallback(({ lat, lng }) => {
+    //     mapRef.current.panTo({ lat, lng });
+    //     mapRef.current.setZoom(14);
+    //   }, []);
+
+    //   if (loadError) return "Error";
+    //   if (!isLoaded) return "Loading...";
     
     return (
         <>
@@ -65,11 +69,19 @@ export default function Antinode() {
                 <meta name="description" content="antinode" />
             </Head>
             <div style={{ position: 'absolute' }}>
-                <Button style={{ background: 'blue', color: 'white' }}>
+                <Button
+                    style={{ background: 'blue', color: 'white' }}
+                >
                     Antinode
                 </Button>
             </div>
-            <div style={{ fontWeight: '300', color: 'white', transform: 'translateY(40px)' }}>
+            <div>
+                <NodeMap />
+            </div>
+            <div>
+                <AntinodeMap />
+            </div>
+            {/* <div style={{ fontWeight: '300', color: 'white', transform: 'translateY(40px)' }}>
                 <div style={{ background: 'white', opacity: '0.4' }}>
                     <Divider />
                 </div>
@@ -80,15 +92,16 @@ export default function Antinode() {
                     Antinode Location
                 </div>
             </div>
+            <div style={{ transform: 'translate(100px, 0px)', position: 'absolute', zIndex: '10' }}>
+                <Locate panTo={panTo} />
+            </div>
+            <div style={{ transform: 'translate(300px, 00px)', position: 'absolute', zIndex: '10' }}>
+                <AntinodeLocate panTo={panTo} />
+            </div>
             <div style={{ transform: 'translateY(-299px)' }}>
                 <div style={{ position: 'absolute', zIndex: '1000000000', width: '50%', height: '100%' }}>
-                    {/* <Map /> */}
-                    <>
-                        <div style={{ transform: 'translate(500px, 800px)', position: 'absolute', zIndex: '10' }}>
-                        <Locate panTo={panTo} />
-                        </div>
-                        <div style={{ display: 'flex', justifyContent: 'center', transform: 'translateY(364px)' }}>
-                            <GoogleMap
+                    <div style={{ display: 'flex', justifyContent: 'center', transform: 'translateY(364px)' }}>
+                        <GoogleMap
                             id="map"
                             mapContainerStyle={mapContainerStyle}
                             zoom={10}
@@ -96,48 +109,42 @@ export default function Antinode() {
                             options={options}
                             onClick={onMapClick}
                             onLoad={onMapLoad}
-                            >
-                            {markers.map((marker) => (
-                                <Marker
-                                key={`${marker.lat}-${marker.lng}`}
-                                position={{ lat: marker.lat, lng: marker.lng }}
-                                onClick={() => {
-                                    setSelected(marker);
-                                }}
-                                icon={{
-                                    origin: new window.google.maps.Point(0, 0),
-                                    anchor: new window.google.maps.Point(15, 15),
-                                    scaledSize: new window.google.maps.Size(30, 30),
-                                }}
-                                />
-                            ))}
+                        >
+                        {markers.map((marker) => (
+                            <Marker
+                            key={`${marker.lat}-${marker.lng}`}
+                            position={{ lat: marker.lat, lng: marker.lng }}
+                            onClick={() => {
+                                setSelected(marker);
+                            }}
+                            icon={{
+                                origin: new window.google.maps.Point(0, 0),
+                                anchor: new window.google.maps.Point(15, 15),
+                                scaledSize: new window.google.maps.Size(30, 30),
+                            }}
+                            />
+                        ))}
 
-                            {selected ? (
-                                <InfoWindow
-                                position={{ lat: selected.lat, lng: selected.lng }}
-                                onCloseClick={() => {
-                                    setSelected(null);
-                                }}
-                                >
-                                <div>
-                                    <h2>
-                                    Dropped Pin
-                                    </h2>
-                                </div>
-                                </InfoWindow>
-                            ) : null}
-                            </GoogleMap>
-                        </div>
-                    </>
+                        {selected ? (
+                            <InfoWindow
+                            position={{ lat: selected.lat, lng: selected.lng }}
+                            onCloseClick={() => {
+                                setSelected(null);
+                            }}
+                            >
+                            <div>
+                                <h2>
+                                Dropped Pin
+                                </h2>
+                            </div>
+                            </InfoWindow>
+                        ) : null}
+                        </GoogleMap>
+                    </div>
                 </div>
                 <div style={{ position: 'absolute', zIndex: '1000000000', width: '50%', height: '100%', transform: 'translate(100%)' }}>
-                    {/* <Map /> */}
-                    <>
-                        <div style={{ transform: 'translate(500px, 800px)', position: 'absolute', zIndex: '10' }}>
-                            <Locate panTo={panTo} />
-                        </div>
-                        <div style={{ display: 'flex', justifyContent: 'center', transform: 'translateY(364px)' }}>
-                            <GoogleMap
+                    <div style={{ display: 'flex', justifyContent: 'center', transform: 'translateY(364px)' }}>
+                        <GoogleMap
                             id="map"
                             mapContainerStyle={mapContainerStyle}
                             zoom={10}
@@ -145,86 +152,92 @@ export default function Antinode() {
                             options={options}
                             onClick={onMapClick}
                             onLoad={onMapLoad}
-                            >
-                            {markers.map((marker) => (
-                                <Marker
-                                key={`${marker.lat}-${marker.lng}`}
-                                position={{ lat: marker.lat, lng: marker.lng }}
-                                onClick={() => {
-                                    setSelected(marker);
-                                }}
-                                icon={{
-                                    origin: new window.google.maps.Point(0, 0),
-                                    anchor: new window.google.maps.Point(15, 15),
-                                    scaledSize: new window.google.maps.Size(30, 30),
-                                }}
-                                />
-                            ))}
+                        >
+                        {markers.map((marker) => (
+                            <Marker
+                            key={`${marker.lat}-${marker.lng}`}
+                            position={{ lat: marker.lat, lng: marker.lng }}
+                            onClick={() => {
+                                setSelected(marker);
+                            }}
+                            icon={{
+                                origin: new window.google.maps.Point(0, 0),
+                                anchor: new window.google.maps.Point(15, 15),
+                                scaledSize: new window.google.maps.Size(30, 30),
+                            }}
+                            />
+                        ))}
 
-                            {selected ? (
-                                <InfoWindow
-                                position={{ lat: selected.lat, lng: selected.lng }}
-                                onCloseClick={() => {
-                                    setSelected(null);
-                                }}
-                                >
-                                <div>
-                                    <h2>
-                                    Dropped Pin
-                                    </h2>
-                                </div>
-                                </InfoWindow>
-                            ) : null}
-                            </GoogleMap>
-                        </div>
-                    </>
+                        {selected ? (
+                            <InfoWindow
+                            position={{ lat: selected.lat, lng: selected.lng }}
+                            onCloseClick={() => {
+                                setSelected(null);
+                            }}
+                            >
+                            <div>
+                                <h2>
+                                Dropped Pin
+                                </h2>
+                            </div>
+                            </InfoWindow>
+                        ) : null}
+                        </GoogleMap>
+                    </div>
                 </div>
-            </div>
+            </div> */}
         </>
     );
 }
 
-function Locate({ panTo }) {
-    return (
-      <>
-        <div>
-          <Button
-            className={styles.button}
-            onClick={() => {
-              navigator.geolocation.getCurrentPosition(
-                (position) => {
-                  panTo({
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                  });
-                },
-                () => null
-              );
-            }}
-          > 
-            <LocationOnIcon fontSize="small" />&nbsp;
-            Current Location
-          </Button>
-        </div>
-        <div>
-          <Button
-            className={styles.button}
-            onClick={() => {
-              navigator.geolocation.getCurrentPosition(
-                (position) => {
-                  panTo({
-                    lat: (parseFloat(position.coords.latitude) - (parseFloat(position.coords.latitude) * 2)),
-                    lng: (parseFloat(position.coords.longitude) + 180),
-                  });
-                },
-                () => null
-              );
-            }}
-          >
-            <FlipCameraAndroidIcon fontSize="small" />&nbsp;
-            Opposite Location
-          </Button>
-        </div>
-      </>
-    );
-  }
+// function Locate({ panTo }) {
+//     return (
+//       <>
+//         <div>
+//           <Button
+//             className={styles.button}
+//             onClick={() => {
+//               navigator.geolocation.getCurrentPosition(
+//                 (position) => {
+//                   panTo({
+//                     lat: position.coords.latitude,
+//                     lng: position.coords.longitude,
+//                   });
+//                 },
+//                 () => null
+//               );
+//             }}
+//           > 
+//             <LocationOnIcon fontSize="small" />&nbsp;
+//             Current Location
+//           </Button>
+//         </div>
+//       </>
+//     );
+//   }
+
+// function AntinodeLocate({ panTo }) {
+//     return (
+//         <>
+//             <div>
+//                 <Button
+//                     className={styles.button}
+//                     onClick={() => {
+//                     navigator.geolocation.getCurrentPosition(
+//                         (position) => {
+//                         panTo({
+//                             lat: (parseFloat(position.coords.latitude) - (parseFloat(position.coords.latitude) * 2)),
+//                             lng: (parseFloat(position.coords.longitude) + 180),
+//                         });
+//                         },
+//                         () => null
+//                     );
+//                     }}
+//                 >
+//                     <FlipCameraAndroidIcon fontSize="small" />&nbsp;
+//                     Opposite Location
+//                 </Button>
+//             </div>
+//         </>
+//     );
+// }
