cb446eeeb7017fa1882e19c5a6b53d5d56ad72fa
Jbertalli <jjbertalli@berkeley.edu>
2022-09-16 02:11:45 -0700

    Link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Link from 'next/link';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
@@ -16,7 +17,9 @@ export default function Navbar() {
                 <Tabs value={value} onChange={handleChange} centered>
                     <Tab style={{ color: 'white', fontSize: '25px', fontWeight: '100' }} label="Home" />
                     <Tab style={{ color: 'white', fontSize: '25px', fontWeight: '100' }} label="Current Location" />
-                    <Tab style={{ color: 'white', fontSize: '25px', fontWeight: '100' }} label="Antinode Map" />
+                    <Link href="/antinode_map" passHref>
+                        <Tab style={{ color: 'white', fontSize: '25px', fontWeight: '100' }} label="Antinode Map" />
+                    </Link>
                 </Tabs>
             </Box>
         </>
