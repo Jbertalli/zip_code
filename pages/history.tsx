f14a940e7038076261707692d3e49e605df96232
Jbertalli <jjbertalli@berkeley.edu>
2022-09-17 15:00:19 -0700

    history <Head>

--- pages/history.tsx
+++ pages/history.tsx
@@ -1,6 +1,12 @@
+import Head from 'next/head';
+
 export default function History() {
     return (
         <>
+            <Head>
+                <title>User History</title>
+                <meta name="description" content="history" />
+            </Head>
             Hello
         </>
     );
