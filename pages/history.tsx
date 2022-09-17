688dfa4490b5a9f95c247906f816132431854821
Jbertalli <jjbertalli@berkeley.edu>
2022-09-17 15:09:48 -0700

    history navbar

--- pages/history.tsx
+++ pages/history.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { getAuth } from '@firebase/auth';
 import { initializeApp } from 'firebase/app';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 export default function History() {
 
@@ -23,6 +26,19 @@ export default function History() {
                 <title>User Information</title>
                 <meta name="description" content="history" />
             </Head>
+            <div style={{ position: 'absolute', zIndex: '10000', right: '36vw', top: '5px' }}>
+                <Tabs>
+                    <Link href='/' passHref>
+                        <Tab style={{ color: 'white', fontSize: '25px', fontWeight: '400', textTransform: 'none' }} label="Home" />
+                    </Link>
+                    <Link href='/antinode_map' passHref>
+                        <Tab style={{ color: 'white', fontSize: '25px', fontWeight: '400', textTransform: 'none' }} label="Antinode Map" />
+                    </Link>
+                    <Link href='/history' passHref>
+                        <Tab style={{ color: 'white', fontSize: '25px', fontWeight: '400', textTransform: 'none' }} label="User Information" />
+                    </Link>
+                </Tabs>
+            </div>
             <div style={{ fontSize: '16px', fontWeight: '400', position: 'absolute', zIndex: '10000000', color: 'white', left: '16px', top: '21px' }}>
                 <span>
                     {user ? (
