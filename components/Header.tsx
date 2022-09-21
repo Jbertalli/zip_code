3e2e2f12f8d0f0e07083135ed02d28e7cbddf7cb
Jbertalli <jjbertalli@berkeley.edu>
2022-09-21 03:21:06 -0700

    active route edit

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,22 +8,25 @@ export default function Header() {
     const [homeColor, setHomeColor] = useState('');
     const [antinodeColor, setAntinodeColor] = useState('');
     const [historyColor, setHistoryColor] = useState('');
-    const [homeUnderline, setHomeUnderline] = useState('');
-    const [antinodeUnderline, setAntinodeUnderline] = useState('');
-    const [historyUnderline, setHistoryUnderline] = useState('');
+    // const [homeUnderline, setHomeUnderline] = useState('');
+    // const [antinodeUnderline, setAntinodeUnderline] = useState('');
+    // const [historyUnderline, setHistoryUnderline] = useState('');
+    // textDecoration: `${homeUnderline}`
+    // textDecoration: `${antinodeUnderline}`
+    // textDecoration: `${historyUnderline}`
 
     const router = useRouter();
 
     useEffect(() => {
         if (router.pathname === '/') {
             setHomeColor('#FFFFFF20');
-            setHomeUnderline('underline');
+            // setHomeUnderline('underline');
         } else if (router.pathname === '/antinode_map') {
             setAntinodeColor('#FFFFFF20');
-            setAntinodeUnderline('underline');
+            // setAntinodeUnderline('underline');
         } else if (router.pathname === '/history') {
             setHistoryColor('#FFFFFF20');
-            setHistoryUnderline('underline');
+            // setHistoryUnderline('underline');
         } else {
             return;
         }
@@ -33,13 +36,13 @@ export default function Header() {
         <>
             <Tabs>
                 <Link href='/' passHref>
-                    <Tab style={{ background: `${homeColor}`, textDecoration: `${homeUnderline}`, fontSize: '25px', fontWeight: '400', textTransform: 'none' }} label="Home" />
+                    <Tab style={{ background: `${homeColor}`, fontSize: '25px', fontWeight: '400', textTransform: 'none' }} label="Home" />
                 </Link>
                 <Link href='/antinode_map' passHref>
-                    <Tab style={{ background: `${antinodeColor}`, textDecoration: `${antinodeUnderline}`, fontSize: '25px', fontWeight: '400', textTransform: 'none' }} label="Antinode Map" />
+                    <Tab style={{ background: `${antinodeColor}`, fontSize: '25px', fontWeight: '400', textTransform: 'none' }} label="Antinode Map" />
                 </Link>
                 <Link href='/history' passHref>
-                    <Tab style={{ background: `${historyColor}`, textDecoration: `${historyUnderline}`, fontSize: '25px', fontWeight: '400', textTransform: 'none' }} label="User Information" />
+                    <Tab style={{ background: `${historyColor}`, fontSize: '25px', fontWeight: '400', textTransform: 'none' }} label="User Information" />
                 </Link>
             </Tabs>
         </>
