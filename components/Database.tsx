f11b62ce95363be1c7be09720a768b3145011dcc
Jbertalli <jjbertalli@berkeley.edu>
2022-09-23 21:22:36 -0700

    delete Database component comments

--- components/Database.tsx
+++ components/Database.tsx
@@ -11,16 +11,6 @@ import SyncAltIcon from '@mui/icons-material/SyncAlt';
 import PlaceIcon from '@mui/icons-material/Place';
 import LocalDB from '../components/localStorageDB';
 
-// const LOCAL_STORAGE_KEY_UPDATE_ZIP = 'UpdateZip';
-// const LOCAL_STORAGE_KEY_UPDATE_CITY = 'UpdateCity';
-// const LOCAL_STORAGE_KEY_UPDATE_LATITUDE = 'UpdateLatitude';
-// const LOCAL_STORAGE_KEY_UPDATE_LONGITUDE = 'UpdateLongitude';
-// const LOCAL_STORAGE_KEY_UPDATE_LATITUDE_ANTINODE = 'UpdateAntinodeLatitude';
-// const LOCAL_STORAGE_KEY_UPDATE_LONGITUDE_ANTINODE = 'UpdateAntinodeLongitude';
-// const LOCAL_STORAGE_KEY_UPDATE_STATE = 'UpdateState';
-// const LOCAL_STORAGE_KEY_UPDATE_STATE_ABBREVIATION = 'UpdateStateAbbreviation';
-// const LOCAL_STORAGE_KEY_UPDATE_ALL = 'UpdateAll';
-
 export default function Database({ latCoord, longCoord, zip, city, state, stateAbbreviation, addZip, addCity, addLat, latitude, addLong, longitude, addState, addStateAbbr, addDocument, deleteZip, addOppLat, OppLat, addOppLong, OppLong, dbId, dbZip, dbCity, dbLatitude, dbLongitude, dbOppositeLatitude, dbOppositeLongitude, dbState, dbStateAbbreviation, deleteCity, deleteLat, deleteLong, deleteOppositeLat, deleteOppositeLong, deleteState, deleteAbbr, deleteAll }) {
     const [updateZip, setUpdateZip] = useState(null);
     const [updateCity, setUpdateCity] = useState(null);
@@ -41,96 +31,6 @@ export default function Database({ latCoord, longCoord, zip, city, state, stateA
     console.log(dbOppositeLongitude);
     console.log(dbState);
     console.log(dbStateAbbreviation);
-
-    // useEffect(() => {
-    //     const storedUpdateZip = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_ZIP))
-    //     if (storedUpdateZip) setUpdateZip(storedUpdateZip)
-    //   }, []);
-    
-    // useEffect(() => {
-    //     localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_ZIP, 
-    //     JSON.stringify(updateZip))
-    // }, [updateZip]);
-
-    // useEffect(() => {
-    //     const storedUpdateCity = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_CITY))
-    //     if (storedUpdateCity) setUpdateCity(storedUpdateCity)
-    //   }, []);
-    
-    // useEffect(() => {
-    //     localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_CITY, 
-    //     JSON.stringify(updateCity))
-    // }, [updateCity]);
-
-    // useEffect(() => {
-    //     const storedUpdateLatitude = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_LATITUDE))
-    //     if (storedUpdateLatitude) setUpdateLatitude(storedUpdateLatitude)
-    //   }, []);
-    
-    // useEffect(() => {
-    //     localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_LATITUDE, 
-    //     JSON.stringify(updateLatitude))
-    // }, [updateLatitude]);
-
-    // useEffect(() => {
-    //     const storedUpdateLongitude = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_LONGITUDE))
-    //     if (storedUpdateLongitude) setUpdateLongitude(storedUpdateLongitude)
-    //   }, []);
-    
-    // useEffect(() => {
-    //     localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_LONGITUDE, 
-    //     JSON.stringify(updateLongitude))
-    // }, [updateLongitude]);
-
-    // useEffect(() => {
-    //     const storedUpdateAntinodeLatitude = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_LATITUDE_ANTINODE))
-    //     if (storedUpdateAntinodeLatitude) setUpdateAntinodeLatitude(storedUpdateAntinodeLatitude)
-    //   }, []);
-    
-    // useEffect(() => {
-    //     localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_LATITUDE_ANTINODE, 
-    //     JSON.stringify(updateAntinodeLatitude))
-    // }, [updateAntinodeLatitude]);
-
-    // useEffect(() => {
-    //     const storedUpdateAntinodeLongitude = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_LONGITUDE_ANTINODE))
-    //     if (storedUpdateAntinodeLongitude) setUpdateAntinodeLongitude(storedUpdateAntinodeLongitude)
-    //   }, []);
-    
-    // useEffect(() => {
-    //     localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_LONGITUDE_ANTINODE, 
-    //     JSON.stringify(updateAntinodeLongitude))
-    // }, [updateAntinodeLongitude]);
-
-    // useEffect(() => {
-    //     const storedUpdateState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_STATE))
-    //     if (storedUpdateState) setUpdateState(storedUpdateState)
-    //   }, []);
-    
-    // useEffect(() => {
-    //     localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_STATE, 
-    //     JSON.stringify(updateState))
-    // }, [updateState]);
-
-    // useEffect(() => {
-    //     const storedUpdateStateAbbreviation = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_STATE_ABBREVIATION))
-    //     if (storedUpdateStateAbbreviation) setUpdateStateAbbreviation(storedUpdateStateAbbreviation)
-    //   }, []);
-    
-    // useEffect(() => {
-    //     localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_STATE_ABBREVIATION, 
-    //     JSON.stringify(updateStateAbbreviation))
-    // }, [updateStateAbbreviation]);
-
-    // useEffect(() => {
-    //     const storedUpdateAll = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_ALL))
-    //     if (storedUpdateAll) setUpdateAll(storedUpdateAll)
-    //   }, []);
-    
-    // useEffect(() => {
-    //     localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_ALL, 
-    //     JSON.stringify(updateAll))
-    // }, [updateAll]);
 
     return (
         <>
