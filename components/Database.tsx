89ef3865e1b3323cb7cd3277e666f6cfacc87b95
Jbertalli <jjbertalli@berkeley.edu>
2022-09-23 20:44:05 -0700

    localStorageDB zip

--- components/Database.tsx
+++ components/Database.tsx
@@ -9,8 +9,9 @@ import LocationCityIcon from '@mui/icons-material/LocationCity';
 import HeightIcon from '@mui/icons-material/Height';
 import SyncAltIcon from '@mui/icons-material/SyncAlt';
 import PlaceIcon from '@mui/icons-material/Place';
+import LocalDB from '../components/localStorageDB';
 
-const LOCAL_STORAGE_KEY_UPDATE_ZIP = 'UpdateZip';
+// const LOCAL_STORAGE_KEY_UPDATE_ZIP = 'UpdateZip';
 const LOCAL_STORAGE_KEY_UPDATE_CITY = 'UpdateCity';
 const LOCAL_STORAGE_KEY_UPDATE_LATITUDE = 'UpdateLatitude';
 const LOCAL_STORAGE_KEY_UPDATE_LONGITUDE = 'UpdateLongitude';
@@ -41,15 +42,15 @@ export default function Database({ latCoord, longCoord, zip, city, state, stateA
     console.log(dbState);
     console.log(dbStateAbbreviation);
 
-    useEffect(() => {
-        const storedUpdateZip = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_ZIP))
-        if (storedUpdateZip) setUpdateZip(storedUpdateZip)
-      }, []);
+    // useEffect(() => {
+    //     const storedUpdateZip = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_ZIP))
+    //     if (storedUpdateZip) setUpdateZip(storedUpdateZip)
+    //   }, []);
     
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_ZIP, 
-        JSON.stringify(updateZip))
-    }, [updateZip]);
+    // useEffect(() => {
+    //     localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_ZIP, 
+    //     JSON.stringify(updateZip))
+    // }, [updateZip]);
 
     useEffect(() => {
         const storedUpdateCity = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_CITY))
@@ -142,179 +143,182 @@ export default function Database({ latCoord, longCoord, zip, city, state, stateA
     // console.log(updateAll);
 
     return (
-        <List>
-            {/* {!!dbZip ? ( */}
-            {updateZip ? (
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {deleteZip(zip), setUpdateZip(false)}}>
-                        <ClearIcon fontSize="small" />&nbsp;
-                        Delete Zip
-                    </ListItemButton>
-                </ListItem>
-            </>
-            ):(
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {addZip(zip), setUpdateZip(true)}}>
-                        <TagIcon fontSize="small" />&nbsp;
-                        Save Zip
-                    </ListItemButton>
-                </ListItem>
-            </>
-            )}
-            {updateCity ? (
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {deleteCity(city), setUpdateCity(false)}}>
-                        <ClearIcon fontSize="small" />&nbsp;
-                        Delete City
-                    </ListItemButton>
-                </ListItem>
-            </>
-            ):(
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {addCity(city), setUpdateCity(true)}}>
-                        <LocationCityIcon fontSize="small" />&nbsp;
-                        Save City
-                    </ListItemButton>
-                </ListItem>
-            </>
-            )}
-            {updateLatitude ? (
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {deleteLat(latitude), setUpdateLatitude(false)}}>
-                        <ClearIcon fontSize="small" />&nbsp;
-                        Delete Latitude
-                    </ListItemButton>
-                </ListItem>
-            </>
-            ):(
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {addLat(latitude), setUpdateLatitude(true)}}>
-                        <HeightIcon fontSize="small" style={{ transform: 'rotate(90deg)' }} />&nbsp;
-                        Save Latitude
-                    </ListItemButton>
-                </ListItem>
-            </>
-            )}
-            {updateLongitude ? (
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {deleteLong(longitude), setUpdateLongitude(false)}}>
-                        <ClearIcon fontSize="small" />&nbsp;
-                        Delete Longitude
-                    </ListItemButton>
-                </ListItem>
-            </>
-            ):(
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {addLong(longitude), setUpdateLongitude(true)}}>
-                        <HeightIcon fontSize="small" />&nbsp;
-                        Save Longitude
-                    </ListItemButton>
-                </ListItem>
-            </>
-            )}
-            {updateAntinodeLatitude ? (
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {deleteOppositeLat(OppLat), setUpdateAntinodeLatitude(false)}}>
-                        <ClearIcon fontSize="small" />&nbsp;
-                        Delete Antinode Latitude
-                    </ListItemButton>
-                </ListItem>
-            </>
-            ):(
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {addOppLat(OppLat), setUpdateAntinodeLatitude(true)}}>
-                        <SyncAltIcon fontSize="small" />&nbsp;
-                        Save Antinode Latitude
-                    </ListItemButton>
-                </ListItem>
-            </>
-            )}
-            {updateAntinodeLongitude ? (
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {deleteOppositeLong(OppLong), setUpdateAntinodeLongitude(false)}}>
-                        <ClearIcon fontSize="small" />&nbsp;
-                        Delete Antinode Longitude
-                    </ListItemButton>
-                </ListItem>
-            </>
-            ):(
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {addOppLong(OppLong), setUpdateAntinodeLongitude(true)}}>
-                        <SyncAltIcon fontSize="small" style={{ transform: 'rotate(90deg)' }} />&nbsp;
-                        Save Antinode Longitude
-                    </ListItemButton>
-                </ListItem>
-            </>
-            )}
-            {updateState ? (
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {deleteState(state), setUpdateState(false)}}>
-                        <ClearIcon fontSize="small" />&nbsp;
-                        Delete State
-                    </ListItemButton>
-                </ListItem>
-            </>
-            ):(
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {addState(state), setUpdateState(true)}}>
-                        <PlaceIcon fontSize="small" />&nbsp;
-                        Save State
-                    </ListItemButton>
-                </ListItem>
-            </>
-            )}
-            {updateStateAbbreviation ? (
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {deleteAbbr(stateAbbreviation), setUpdateStateAbbreviation(false)}}>
-                        <ClearIcon fontSize="small" />&nbsp;
-                        Delete State Abbreviation
-                    </ListItemButton>
-                </ListItem>
-            </>
-            ):(
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {addStateAbbr(stateAbbreviation), setUpdateStateAbbreviation(true)}}>
-                        <PlaceIcon fontSize="small" style={{ transform: 'scale(0.7)'}} />&nbsp;
-                        Save State Abbreviation
-                    </ListItemButton>
-                </ListItem>
-            </>
-            )}
-            {updateAll ? (
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {deleteAll(zip, city, latCoord, longCoord, state, stateAbbreviation), setUpdateAll(false)}}>
-                        <ClearIcon fontSize="small" />&nbsp;
-                        Delete All
-                    </ListItemButton>
-                </ListItem>
-            </>
-            ):(
-            <>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => {addDocument(zip, city, latCoord, longCoord, OppLat, OppLong, state, stateAbbreviation), setUpdateAll(true)}}>
-                        <SaveAltIcon fontSize="small" />&nbsp;
-                        Save All
-                    </ListItemButton>
-                </ListItem>
-            </>
-            )}
-        </List>
+        <>
+            <LocalDB updateZip={updateZip} setUpdateZip={setUpdateZip} />
+            <List>
+                {/* {!!dbZip ? ( */}
+                {updateZip ? (
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {deleteZip(zip), setUpdateZip(false)}}>
+                            <ClearIcon fontSize="small" />&nbsp;
+                            Delete Zip
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                ):(
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {addZip(zip), setUpdateZip(true)}}>
+                            <TagIcon fontSize="small" />&nbsp;
+                            Save Zip
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                )}
+                {updateCity ? (
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {deleteCity(city), setUpdateCity(false)}}>
+                            <ClearIcon fontSize="small" />&nbsp;
+                            Delete City
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                ):(
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {addCity(city), setUpdateCity(true)}}>
+                            <LocationCityIcon fontSize="small" />&nbsp;
+                            Save City
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                )}
+                {updateLatitude ? (
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {deleteLat(latitude), setUpdateLatitude(false)}}>
+                            <ClearIcon fontSize="small" />&nbsp;
+                            Delete Latitude
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                ):(
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {addLat(latitude), setUpdateLatitude(true)}}>
+                            <HeightIcon fontSize="small" style={{ transform: 'rotate(90deg)' }} />&nbsp;
+                            Save Latitude
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                )}
+                {updateLongitude ? (
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {deleteLong(longitude), setUpdateLongitude(false)}}>
+                            <ClearIcon fontSize="small" />&nbsp;
+                            Delete Longitude
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                ):(
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {addLong(longitude), setUpdateLongitude(true)}}>
+                            <HeightIcon fontSize="small" />&nbsp;
+                            Save Longitude
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                )}
+                {updateAntinodeLatitude ? (
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {deleteOppositeLat(OppLat), setUpdateAntinodeLatitude(false)}}>
+                            <ClearIcon fontSize="small" />&nbsp;
+                            Delete Antinode Latitude
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                ):(
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {addOppLat(OppLat), setUpdateAntinodeLatitude(true)}}>
+                            <SyncAltIcon fontSize="small" />&nbsp;
+                            Save Antinode Latitude
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                )}
+                {updateAntinodeLongitude ? (
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {deleteOppositeLong(OppLong), setUpdateAntinodeLongitude(false)}}>
+                            <ClearIcon fontSize="small" />&nbsp;
+                            Delete Antinode Longitude
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                ):(
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {addOppLong(OppLong), setUpdateAntinodeLongitude(true)}}>
+                            <SyncAltIcon fontSize="small" style={{ transform: 'rotate(90deg)' }} />&nbsp;
+                            Save Antinode Longitude
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                )}
+                {updateState ? (
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {deleteState(state), setUpdateState(false)}}>
+                            <ClearIcon fontSize="small" />&nbsp;
+                            Delete State
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                ):(
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {addState(state), setUpdateState(true)}}>
+                            <PlaceIcon fontSize="small" />&nbsp;
+                            Save State
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                )}
+                {updateStateAbbreviation ? (
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {deleteAbbr(stateAbbreviation), setUpdateStateAbbreviation(false)}}>
+                            <ClearIcon fontSize="small" />&nbsp;
+                            Delete State Abbreviation
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                ):(
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {addStateAbbr(stateAbbreviation), setUpdateStateAbbreviation(true)}}>
+                            <PlaceIcon fontSize="small" style={{ transform: 'scale(0.7)'}} />&nbsp;
+                            Save State Abbreviation
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                )}
+                {updateAll ? (
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {deleteAll(zip, city, latCoord, longCoord, state, stateAbbreviation), setUpdateAll(false)}}>
+                            <ClearIcon fontSize="small" />&nbsp;
+                            Delete All
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                ):(
+                <>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => {addDocument(zip, city, latCoord, longCoord, OppLat, OppLong, state, stateAbbreviation), setUpdateAll(true)}}>
+                            <SaveAltIcon fontSize="small" />&nbsp;
+                            Save All
+                        </ListItemButton>
+                    </ListItem>
+                </>
+                )}
+            </List>
+        </>
     );
 }
--- components/localStorageDB.tsx
+++ components/localStorageDB.tsx
@@ -1,7 +1,22 @@
-export default function LocalDB() {
+import React, { useEffect } from 'react';
+
+const LOCAL_STORAGE_KEY_UPDATE_ZIP = 'UpdateZip';
+
+export default function LocalDB({ updateZip, setUpdateZip }) {
+
+    useEffect(() => {
+        const storedUpdateZip = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_UPDATE_ZIP))
+        if (storedUpdateZip) setUpdateZip(storedUpdateZip)
+      }, []);
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_UPDATE_ZIP, 
+        JSON.stringify(updateZip))
+    }, [updateZip]);
+
     return (
         <>
-            Hello
+            &nbsp;
         </>
     );
 }
