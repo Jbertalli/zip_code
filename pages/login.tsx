1de1d786a5b25e9c18b914db0255063d4d685c3e
Jbertalli <jjbertalli@berkeley.edu>
2022-09-30 23:08:03 -0700

    login Head

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import Header from '../components/Header';
 
 export default function Login() {
 
     return (
         <>  
+            <Head>
+                <title>Login</title>
+                <meta name="description" content="login" />
+            </Head>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <div style={{ position: 'absolute', zIndex: '10000', top: '5px' }}>
                     <Header />
