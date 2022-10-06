96a364f6c2653fd960f2d6bd20d2bf6c29f8c00a
Jbertalli <jjbertalli@berkeley.edu>
2022-10-06 13:59:22 -0700

    implement ternary

--- pages/antinode_map.tsx
+++ pages/antinode_map.tsx
@@ -33,33 +33,32 @@ export default function Antinode() {
             </Head>
             {isDesktop ? (
             <>
-
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <div style={{ position: 'absolute', zIndex: '10000', top: '5px' }}>
+                        <Header />
+                    </div>
+                </div>
+                <div style={{ background: '#313e4c', width: '100%', height: '20px', position: 'absolute', zIndex: '10', top: '65px', borderTop: '.5px solid #FFFFFF90' }} />
+                <div style={{ fontSize: '15px', fontWeight: '300', opacity: '0.8', transform: 'translateY(65.5px)', position: 'absolute', zIndex: '10' }}>
+                    <div style={{ position: 'absolute', transform: 'translate(20vw)', width: '300px' }}>
+                        Current Location
+                    </div>
+                    <div style={{ position: 'absolute', transform: 'translate(70vw)', width: '300px' }}>
+                        Antinode Location
+                    </div>
+                </div>
+                <div>
+                    <NodeMap />
+                </div>
+                <div>
+                    <AntinodeMap />
+                </div>
             </>
             ):(
             <>
-
+                Hello
             </>
             )}
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <div style={{ position: 'absolute', zIndex: '10000', top: '5px' }}>
-                    <Header />
-                </div>
-            </div>
-            <div style={{ background: '#313e4c', width: '100%', height: '20px', position: 'absolute', zIndex: '10', top: '65px', borderTop: '.5px solid #FFFFFF90' }} />
-            <div style={{ fontSize: '15px', fontWeight: '300', opacity: '0.8', transform: 'translateY(65.5px)', position: 'absolute', zIndex: '10' }}>
-                <div style={{ position: 'absolute', transform: 'translate(20vw)', width: '300px' }}>
-                    Current Location
-                </div>
-                <div style={{ position: 'absolute', transform: 'translate(70vw)', width: '300px' }}>
-                    Antinode Location
-                </div>
-            </div>
-            <div>
-                <NodeMap />
-            </div>
-            <div>
-                <AntinodeMap />
-            </div>
         </>
     );
 }
